4dfa23352d93c436bbdfd5d5a4c7821a114fd290
Shreya Raj <shreyaraj416@gmail.com>
2026-02-21 22:08:07 +0530

    complete sign up page functionality

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -4,5 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server:{
+    proxy:{
+      '/back-end':{
+        target:'http://localhost:3000',
+        secure:false,
+      },
+    },
+  },
   plugins: [react(),tailwindcss()],
 })
