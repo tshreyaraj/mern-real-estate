fe5c88562100a377b3b54a0ab33c215c81350700
Shreya Raj <shreyaraj416@gmail.com>
2026-02-22 22:17:27 +0530

    complete sign in page functionality

--- front-end/src/pages/SignUp.jsx
+++ front-end/src/pages/SignUp.jsx
@@ -48,9 +48,9 @@ export default function SignUp() {
     <div className='p-3 max-w-lg mx-auto'>
      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
      <form className='flex flex-col gap-4 ' onSubmit={handleSubmit}>
-      <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handleChange}/>
-      <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
-      <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
+      <input type="text" placeholder='username' className='bg-white p-3 rounded-lg' id='username' onChange={handleChange}/>
+      <input type="email" placeholder='email' className='bg-white p-3 rounded-lg' id='email' onChange={handleChange}/>
+      <input type="password" placeholder='password' className='bg-white p-3 rounded-lg' id='password' onChange={handleChange}/>
       <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...':'Sign Up'}</button>
      </form>
      <div className="flex gap-2 mt-5">
