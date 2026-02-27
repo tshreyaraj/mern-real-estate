adbd9b9a03447a27ee8382fa798d926dedb18e6d
Shreya Raj <shreyaraj416@gmail.com>
2026-02-27 15:15:06 +0530

    create and add OAuth component

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  plugins: [react(),tailwindcss()],
   server:{
     proxy:{
       '/back-end':{
@@ -12,5 +13,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react(),tailwindcss()],
+  
 })
