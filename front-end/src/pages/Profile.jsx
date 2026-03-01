17ab2a0d76667abd1d7d2fc8073f53338333a3a0
Shreya Raj <shreyaraj416@gmail.com>
2026-03-01 12:40:33 +0530

    create update user  api

--- front-end/src/pages/Profile.jsx
+++ front-end/src/pages/Profile.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useRef } from "react";
 
 export default function Profile() {
+  const fileRef=useRef(null);
   const { currentUser } = useSelector((state) => state.user);
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl front-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-4">
+       <input type="file" ref={fileRef} hidden accept='image/*'/>
         <img
+          onClick={()=>fileRef.current.click()}
           src={currentUser.avatar}
           alt="profile"
           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center"
+    
         ></img>
         <input
           type="text"
